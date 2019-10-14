f2e4bb48528704b9fe934cbcc148116c478651ef
Santiago Mondragón Gómez-main <santiagomondragon67@gmail.com>
2019-10-14 10:56:07 -0500

    started over

--- p1-dogProgram.c
+++ p1-dogProgram.c
@@ -14,7 +14,7 @@ void buscar();
 
 struct dogType{
   char nombre[33];
-  char tipo[17];
+  char tipo[33];
   int edad;
   char raza[17];
   int estatura;
@@ -24,15 +24,13 @@ struct dogType{
 
 typedef struct entry_t {
     char *key;
-    struct dogType *value;
+    int value;
     struct entry_t *next;
 } entry_t;
 
 typedef struct {
     entry_t **entries;
 } ht_t;
-
-ht_t *ht;
 
 unsigned int hash(const char *key) {
     unsigned long int value = 0;
@@ -50,15 +48,15 @@ unsigned int hash(const char *key) {
     return value;
 }
 
-entry_t *ht_pair(const char *key, struct dogType *value) {
+entry_t *ht_pair(const char *key, int value) {
     // allocate the entry
     entry_t *entry = malloc(sizeof(entry_t) * 1);
     entry->key = malloc(strlen(key) + 1);
-    entry->value = malloc(sizeof(struct dogType));
+    //entry->value = malloc(sizeof(int));
 
     // copy the key and value in place
     strcpy(entry->key, key);
-    entry -> value = value;
+    entry->value = value;
 
     // next starts out null but may be set later on
     entry->next = NULL;
@@ -82,7 +80,7 @@ ht_t *ht_create(void) {
     return hashtable;
 }
 
-void ht_set(ht_t *hashtable, const char *key, struct dogType *value) {
+void ht_set(ht_t *hashtable, const char *key, int value) {
     unsigned int slot = hash(key);
 
     // try to look up an entry set
@@ -99,14 +97,6 @@ void ht_set(ht_t *hashtable, const char *key, struct dogType *value) {
     // walk through each entry until either the end is
     // reached or a matching key is found
     while (entry != NULL) {
-        // check key
-        // if (strcmp(entry->key, key) == 0) {
-        //     // match found, replace value
-        //     free(entry->value);
-        //     entry->value = malloc(sizeof(struct dogType) + 1);
-        //     entry -> value = value;
-        //     return;
-        // }
 
         // walk to next
         prev = entry;
@@ -119,31 +109,28 @@ void ht_set(ht_t *hashtable, const char *key, struct dogType *value) {
 
 void ht_get(ht_t *hashtable, const char *key) {
     unsigned int slot = hash(key);
-    int count = 0;
+
     // try to find a valid slot
     entry_t *entry = hashtable->entries[slot];
 
+    // no slot means no entry
     if (entry == NULL) {
-      printf("No existen coincidencias.\n");
+        //print hey theres nothing
         return;
     }
 
     // walk through each entry in the slot, which could just be a single thing
     while (entry != NULL) {
-        // return value if found
+        // print value if found
         if (strcmp(entry->key, key) == 0) {
-            printf("hey encontre algo\n");
-            printf("Nombre: %s\n", entry -> value -> nombre);
-            printf("Tipo: %s\n", entry -> value -> tipo);
-            printf("Raza: %s\n", entry -> value -> raza);
-            count++;
+            //print something
         }
 
         // proceed to next key if available
         entry = entry->next;
     }
 
-    printf("Encontradas %i coincidencias\n", count);
+    // print number of coincidences
     return;
 }
 
@@ -187,7 +174,7 @@ void ht_del(ht_t *hashtable, const char *key) {
 
             // free the deleted entry
             free(entry->key);
-            free(entry->value);
+            //free(entry->value);
             free(entry);
 
             return;
@@ -212,7 +199,7 @@ void ht_dump(ht_t *hashtable) {
         printf("slot[%4d]: ", i);
 
         for(;;) {
-            printf("%s=%s ", entry->key, entry->value->raza);
+            printf("%s=%i ", entry->key, entry->value);
 
             if (entry->next == NULL) {
                 break;
@@ -223,20 +210,6 @@ void ht_dump(ht_t *hashtable) {
 
         printf("\n");
     }
-}
-
-struct dogType *copyStruct(struct dogType *src){
-  struct dogType *new_dog = (struct dogType*)malloc(sizeof(struct dogType));
-
-  strcpy(new_dog -> nombre, src -> nombre);
-  strcpy(new_dog -> tipo , src -> tipo);
-  new_dog -> edad = src -> edad;
-  strcpy(new_dog -> raza , src -> raza);
-  new_dog -> estatura = src -> estatura;
-  new_dog -> peso = src -> peso;
-  new_dog -> sexo = src -> sexo;
-
-  return new_dog;
 }
 
 void ingresar(){
@@ -283,9 +256,6 @@ void ingresar(){
     exit(-1);
   }
 
-  struct dogType *new_patient = copyStruct(animal);
-  ht_set(ht, new_patient -> nombre, new_patient);
-
   int write_result = fwrite(animal, sizeof(struct dogType), 1, current_file);
 
   if(write_result != 1){
@@ -296,9 +266,12 @@ void ingresar(){
   fclose(current_file);
   free(animal);
 
+
+  printf("Presione cualquier tecla...");
   getchar();
   getchar();
-  printf("Escritura completada.");
+  printf("Hecho!\n");
+  printf("--");
 }
 
 void ver(){
@@ -314,7 +287,7 @@ void buscar(){
   printf("Digite el nombre del paciente a buscar: ");
   scanf(" %[^\t\n]s", search);
 
-  ht_get(ht, search);
+
 
   printf("Hecho!\n");
   printf("Presione cualquier tecla...");
@@ -336,55 +309,53 @@ void loadHash(){
   rewind(current_file);
 
   while(ftell(current_file) < eof){
-    reader = (struct dogType*)malloc(sizeof(struct dogType));
 
-    read_result = fread(reader, sizeof(struct dogType), 1, current_file);
 
-    if(read_result < 1){
-      perror("Fallo en la lectura");
-      exit(-1);
-    }
-
-    struct dogType *new_reader = copyStruct(reader);
-
-    ht_set(ht, new_reader -> nombre, new_reader);
-
-    free(reader);
   }
   printf("Listo.\n");
   fclose(current_file);
 }
 
 int main(){
-  ht = ht_create();
-  char *option = malloc(1);
-  //struct dogType *dog = (struct dogType*)malloc(sizeof(struct dogType));
-
-  current_file = fopen("dataDogs.dat" , "r");
-  //recibir posición del final del archivo
-  fseek(current_file, 0L, SEEK_END);
-  long eof = ftell(current_file);
-  rewind(current_file);
-
-  if(ftell(current_file) != eof){
-    loadHash();
-  }
 
   printf("Sistemas Operativos - Practica 1. Bienvenido.\n");
+  printf("%i\n", sizeof(struct dogType*));
+  printf("%i\n", sizeof(struct dogType));
+  printf("%i", sizeof(int));
 
-  while(1){
-    printf("\n1. Ingresar paciente.\n");
-    printf("2. Ver paciente por numero.\n");
-    printf("3. Borrar paciente.\n");
-    printf("4. Buscar paciente.\n");
-    printf("5. Salir.\n");
-    printf("Seleccione una opcion: ");
-    switch(getchar()){
-      case '1': ingresar(); break;
-      case '2': ver();      break;
-      case '3': borrar();   break;
-      case '4': buscar();   break;
-      case '5': exit(0);
-    }
-  }
+  // while(1){
+  //   printf("\n1. Ingresar paciente.\n");
+  //   printf("2. Ver paciente por numero.\n");
+  //   printf("3. Borrar paciente.\n");
+  //   printf("4. Buscar paciente.\n");
+  //   printf("5. Salir.\n");
+  //   printf("Seleccione una opcion: ");
+  //   switch(getchar()){
+  //     case '1': ingresar(); break;
+  //     case '2': ver();      break;
+  //     case '3': borrar();   break;
+  //     case '4': buscar();   break;
+  //     case '5': exit(0);
+  //   }
+  // }
+
+  ht_t *ht = ht_create();
+
+    ht_set(ht, "name1", 1);
+    ht_set(ht, "name2", 234);
+    ht_set(ht, "name3", 567);
+    ht_set(ht, "name4", 890);
+    ht_set(ht, "name5", 69);
+    ht_set(ht, "name6", 420);
+    ht_set(ht, "Argusito", 666);
+    ht_set(ht, "Argusito", 667);
+    ht_set(ht, "Argusito", 676);
+    ht_set(ht, "Argusito", 766);
+    ht_set(ht, "Argusito", 668);
+    ht_set(ht, "Argusito", 686);
+    ht_set(ht, "Argusito", 866);
+
+    ht_dump(ht);
+
+    return 0;
 }
